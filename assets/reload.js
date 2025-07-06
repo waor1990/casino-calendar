17646bac492ee2e90f29f01547ab4fefb4c094e3
waor1990 <wesley.allegre@gmail.com>
2025-07-06 13:34:01 -0700

    Revert "feat: reload page when clicking title (#52)"
    This reverts commit 3b6b83eb49713317eadf9a5dfeebb376eec389a7.

--- assets/reload.js
+++
@@ -1,9 +0,0 @@
-// Reload the page when the title button is clicked
-window.addEventListener('DOMContentLoaded', () => {
-  const button = document.getElementById('title-refresh-button');
-  if (button) {
-    button.addEventListener('click', () => {
-      window.location.reload();
-    });
-  }
-});
